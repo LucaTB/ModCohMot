d86e75bc388d74c444ee0a463e75bd6ad7b1ea15
Luca T.B <lucatourn@gmail.com>
2023-12-02 14:45:44 -0300

    reformulacion, casi lista

--- src/app/pages/exp1/exp1.component.ts
+++ src/app/pages/exp1/exp1.component.ts
@@ -11,5 +11,15 @@ export class Exp1Component implements OnInit {
 
   ngOnInit(): void {
   }
+showSection1 = true;
+ showSection2 = false;
+ showSection3 = false;
+
+ showSection(sectionId: string) {
+    this.showSection1 = sectionId === 'section1';
+    this.showSection2 = sectionId === 'section2';
+    this.showSection3 = sectionId === 'section3';
+ }
 
 }
+
